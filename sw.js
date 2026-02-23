c02bcd2a01ea161fd7524c9bcf55c03c433bd792
empregaprogresso-eng <empregaprogresso@gmail.com>
2026-02-23 16:39:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = "humiva-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
